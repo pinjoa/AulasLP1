b62c6f891755cd40e11113ddb2562225f167927f
Joao Pinto <6815379+pinjoa@users.noreply.github.com>
2020-10-30 22:00:49 +0000

    20201030 novidades

---
+++ HelloWorld/main.c
@@ -0,0 +1,14 @@
+/**
+ * @author João Carlos Pinto
+ * @version 0.1
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    printf("Hello world!");
+
+    getchar();
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# PI - Programação Imperativa
+## 2020-2021
+
+Trabalhos e exercicios de LP1
